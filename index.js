3fa9cf506718ee99647443e595a13306ec5dc898
Dario Goutto <dario.goutto@gmail.com>
2015-03-29 04:02:43 +0100

    created class layout, created list of accepted currencies, wrote the _isValidCurrency and _isValidAPIKey methods & started working on the convert method

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function currencyConverter ( key ){
-	this.key = key;
+// My API key : 62baa34b0d64b9002beffe2b34415384fa853d69
+function currencyConverter( key ){
+	this.key 		= key;
+	this.currencies = ['AUD', 'CAD','CHF','DKK','EUR','GBP','HKD','JPY','MXN','NZD','PHP','SEK','SGD','THB','USD','ZAR']
 }
 
-
 currencyConverter.prototype = {
-	convert : function(){}
-	_isValidAPIKey : function(){},
-	_isValidCurrency : function(){},
+	convert : function( from, to, amount, cb ){
+		
+		if( !this._isValidAPIKey() || !this._isValidCurrency( from ) || !this._isValidCurrency( to )  )
+			if( typeof cb === 'function' ) cb( new Error("Invalid functions parameters") );
+
+	},
+
+	_isValidCurrency : function( currency ){ return typeof currency != 'string' || this.currencies.indexOf( currency.toUpperCase() ) === -1 ? false : true; },
+	_isValidAPIKey : function(){ return !this.key ? false : true; },
 	_isValidAmount : function(){}
 }
 
+
+new currencyConverter().convert( 'aud', 'usd', 234, function( err ){
+	console.log( err );
+});
 
 // module.export = currencyConverter;
